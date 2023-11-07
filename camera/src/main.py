e540ea2b8561266934cd88dc12d3ba997ce41e49
Groenborg14 <christian1409.g.m@gmail.com>
2023-11-07 09:11:49 +0100

    Trials_but_mostly_errors

--- camera/src/main.py
+++ camera/src/main.py
@@ -1,9 +1,10 @@
 import cv2, time
-from picamera2 import Picamera2, Preview
-from libcamera import ColorSpace
+#from picamera2 import Picamera2, Preview
+#from libcamera import ColorSpace
 from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import colors
 
 def take_pic():
     picam2 = Picamera2()
@@ -24,15 +25,23 @@ def take_pic():
 
 image_center = [int(2464/2),int(3280/2)]    
 
-im = cv2.imread("/home/g751/Desktop/Project/P7-751/camera/src/Image/newimage.jpg")
+#im = cv2.imread("/home/g751/Desktop/Project/P7-751/camera/src/Image/newimage.jpg")
+im = cv2.imread("/home/christian/Drone_project/P7-751/camera/src/Image/newimage.jpg")
 
 im_rgb = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
-im2 = Image.open("/home/g751/Desktop/Project/P7-751/camera/src/Image/newimage.jpg")
-im_blue = cv2.imread("/home/g751/Desktop/Project/P7-751/camera/src/Image/blueimage.jpg")
+
+#im2 = Image.open("/home/g751/Desktop/Project/P7-751/camera/src/Image/newimage.jpg")
+#im_blue = cv2.imread("/home/g751/Desktop/Project/P7-751/camera/src/Image/blueimage.jpg")
+
+im2 = Image.open("/home/christian/Drone_project/P7-751/camera/src/Image/newimage.jpg")
+im_blue = Image.open("/home/christian/Drone_project/P7-751/camera/src/Image/blueimage.jpg")
+im_red = Image.open("/home/christian/Drone_project/P7-751/camera/src/Image/RED.png")
+
 
 im_array = np.array(im_rgb)
 im2_array = np.array(im2)
 im_blue_array = np.array(im_blue)
+im_red_array = np.array(im_red)
 
 im_array_rg = np.array(im_rgb)
 
@@ -87,30 +96,95 @@ print("array 2: ",im2_array)
 
 # plt.imshow(img3,'gray'),plt.show
 
+# w, h = im_blue.shape[:-1]
 
 
-threshold = 0.8
+# threshold = 0.8
 
-im_array_R, im_array_G, im_array_B = cv2.split(im)
-im_blue_array_R, im_blue_array_G, im_blue_array_B = cv2.split(im_blue)
+# im_array_R, im_array_G, im_array_B = cv2.split(im)
+# im_blue_array_R, im_blue_array_G, im_blue_array_B = cv2.split(im_blue)
 
-resultB = cv2.matchTemplate(im_array_R,im_blue_array_R, cv2.TM_SQDIFF)
-resultG = cv2.matchTemplate(im_array_G,im_blue_array_G, cv2.TM_SQDIFF)
-resultR = cv2.matchTemplate(im_array_B,im_blue_array_B, cv2.TM_SQDIFF)
+# resultB = cv2.matchTemplate(im_array_R,im_blue_array_R, cv2.TM_SQDIFF)
+# resultG = cv2.matchTemplate(im_array_G,im_blue_array_G, cv2.TM_SQDIFF)
+# resultR = cv2.matchTemplate(im_array_B,im_blue_array_B, cv2.TM_SQDIFF)
 
-result = resultB + resultG + resultR
 
-print(result)
-loc = np.where(result >= 3*threshold)
-print("loc:",loc)
+# result = resultB + resultG + resultR
+
+
+
+#print("result : ", resultR)
+#print(result)
+#loc = np.where(result >= 3*threshold)
+#print("loc:",loc)
 #print(loc.index(0))
 
-min_idk_x = np.min(loc.index(0)[:])
-max_idk_x = np.max(loc.index(0)[:])
-min_idk_y = np.min(loc.index(1)[:])
-max_idk_y = np.max(loc.index(1)[:])
+# min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(loc)
 
-print(min_idk_x, max_idk_x, min_idk_y, max_idk_y)
+# top_left = min_loc
+# bottom_right = (top_left[0] + w, top_left[1] + h)
+
+# cv2.rectangle(im2_array, top_left, bottom_right, 255 , 2)
+
+# plt.imshow(im2_array)
+# plt.title("detected point"), plt.xticks([]),plt.yticks([])
+# plt.show()
+
+#min_idk_x = np.min(loc.index(0)[:])
+#max_idk_x = np.max(loc.index(0)[:])
+#min_idk_y = np.min(loc.index(1)[:])
+#max_idk_y = np.max(loc.index(1)[:])
+
+#print(min_idk_x, max_idk_x, min_idk_y, max_idk_y)
+
+
+# orb = cv2.ORB_create()
+
+# kp, des = orb.detectAndCompute(im2_array,None)
+# kp_blue, des_blue = orb.detectAndCompute(im_blue_array,None)
+
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING2, crossCheck= True)
+
+# matches_blue = bf.match(des,des_blue)
+
+# matches_blue = sorted(matches_blue, key = lambda x:x.distance)
+
+# img3 = cv2.drawMatches(im2_array,kp,im_blue_array,kp_blue,matches_blue[:1000],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# plt.imshow(img3),plt.show()
+
+
+
+img_blue=(im_blue_array.astype(float))/255.0
+im_blue_hsv = colors.rgb_to_hsv(img_blue[...,:3])
+img_hsv_blue=im_blue_hsv[...,0].flatten()
+                
+plt.hist(img_hsv_blue*360,256,range=(0.0,256), label="Hue")
+plt.show()
+
+img_red=(im_red.astype(float))/255.0
+im_red_hsv = colors.rgb_to_hsv(img_red[...,:3])
+img_hsv_red=im_red_hsv[...,0].flatten()
+                
+plt.hist(img_hsv_red*360,256,range=(0.0,256), label="Hue")
+plt.show()
+
+
+def create_prob_distr(img, histogram, min_saturation, min_value, min_prob):    
+    (h_values,bin_edges,n) = histogram
+    h_values = min_max_scaling(h_values)
+    img = (img.astype(float))/255.0
+    img_hsv = colors.rgb_to_hsv(img[...,:3])
+    prob_distr = np.zeros((img_hsv.shape[0], img_hsv.shape[1]))    
+    for i in range(img_hsv.shape[0]):
+        for j in range(img_hsv.shape[1]):
+            bin_index = np.digitize(img_hsv[i][j][0]*360,bin_edges, right=True)            
+            if(img_hsv[i][j][1] < min_saturation or img_hsv[i][j][2] < min_value or h_values[bin_index-1] < min_prob):                
+                prob_distr[i][j] = 0.0            
+            else:                
+                prob_distr[i][j] = h_values[bin_index-1]
+        return prob_distr
+
 
 
 
@@ -150,16 +224,7 @@ mask_red_filtered = np.transpose(np.nonzero(masked_red_median_opening[:,:,0] > 0
 mask_blue_filtered = np.transpose(np.nonzero(masked_blue_median_opening[:,:,0] > 0)) 
 mask_green_filtered = np.transpose(np.nonzero(masked_green_median_opening[:,:,0] > 0)) 
 mask_yellow_filtered = np.transpose(np.nonzero(masked_yellow_median_opening[:,:,0] > 0)) 
-#print(mask_red_filtered.shape)
 
-# cv2.imshow("blue",masked_blue)
-# cv2.waitKey(0)
-# cv2.imshow("red",masked_red)
-# cv2.waitKey(0)
-# cv2.imshow("green",masked_green)
-# cv2.waitKey(0)
-# cv2.imshow("yellow",masked_yellow)
-# cv2.waitKey(0)
 
 print("Max x:" + str(np.max(mask_red_filtered[:, 0])))
 print("Min x:" + str(np.min(mask_red_filtered[:, 0])))
@@ -220,8 +285,8 @@ res_image_circle = res_image_circle_red + res_image_circle_blue + res_image_circ
 
 #median = cv2.medianBlur(res_image, 5)
 
-cv2.imshow("circle", res_image_circle)
-cv2.waitKey(0)
+#cv2.imshow("circle", res_image_circle)
+#cv2.waitKey(0)
 #cv2.imshow("green_dot",res_image_circle_green)
 #cv2.waitKey(0)
 #cv2.imshow("masked",res_image)
