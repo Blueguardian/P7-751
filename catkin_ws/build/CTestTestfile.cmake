a98a1af368ce7e8e69fc9556ac6c17e5bab9b7b5
Blueguardian <tssa20@student.aau.dk>
2023-11-28 09:40:49 +0100

    Restructure master without catkin and ROS

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/christian/Drone_project/P7-751/catkin_ws/src
-# Build directory: /home/christian/Drone_project/P7-751/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
