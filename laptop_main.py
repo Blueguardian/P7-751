ca58fdc45f57a177c3e8f4cfc58a104a189e3dfe
Blueguardian <tssa20@student.aau.dk>
2023-12-08 20:55:58 +0100

    Added GUI for data interpretation and starting/stopping drone plus for setting desired altitude

--- laptop_main.py
+++ laptop_main.py
@@ -5,6 +5,7 @@ from EKF import EKF
 import multiprocessing, time
 import numpy as np
 from scipy.spatial.transform import Rotation
+from GUI import Drone_GUI
 def EKF_func(vision_pipe, com_pipe):
 
     ekf = EKF()
@@ -36,7 +37,8 @@ def EKF_func(vision_pipe, com_pipe):
                 vision_data[:] = data[:]
             ekf_state = ekf.vision_step(vision_data)
             com_pipe.send(ekf_state)
-        ekf.prediction_step(sensor_data_acc_gyro)
+        ekf_state = ekf.prediction_step(sensor_data_acc_gyro)
+        com_pipe.send(ekf_state)
 
 def Vision(com_pipe, ekf_pipe):
     vision = VisionAlgorithm()
@@ -74,12 +76,25 @@ def Vision(com_pipe, ekf_pipe):
             continue
 
 def Com(vision_pipe, ekf_pipe):
-
+    execution_state = 0
+    desired_altitude = 0
     #Instantiate server object instance
     client = tcp_client(host='192.168.0.101')
+    GUI = Drone_GUI()
     pose = Pose_tracker()
     image_data = None
+
     while True:
+        events, values = GUI.getinput()
+        GUI.SetLED('online', 'red')
+        if events == 'Abort':
+            execution_state = 0
+            GUI.SetLED('online', 'red')
+        elif events == 'Execute':
+            execution_state = 1
+            GUI.SetLED('online', 'green')
+        elif events == 'Submit':
+            desired_altitude = values[1]
         data = client.receiveData() # Receive data
         if not isinstance(data, str):
             origin = data[0]
@@ -96,6 +111,10 @@ def Com(vision_pipe, ekf_pipe):
             data_ekf = None
             while ekf_pipe.poll():
                 data_ekf = ekf_pipe.recv()
+                data_ekf = np.vstack((data_ekf, np.reshape(np.array([execution_state, desired_altitude]), (-1, 1))))
+                GUI.update_text('data', f"x_pos:\t{data_ekf[0]:.6f}\t\tx_vel:\t{data_ekf[3]:.6f}\t\troll:\t{data_ekf[6]:.6f}\n"
+                                        f"y_pos:\t{data_ekf[1]:.6f}\t\ty_vel:\t{data_ekf[4]:.6f}\t\tpitch:\t{data_ekf[7]:.6f}\n"
+                                        f"z_pos:\t{data_ekf[2]:.6f}\t\tz_vel:\t{data_ekf[5]:.6f}\t\tyaw:\t{data_ekf[8]:.6f}")
             client.sendData(data_ekf, 'EKF')
 
 
